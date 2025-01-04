ddb27354890cf05339ba4503c47264b24b141cab
swingsofwindy <22521717@gm.uit.edu.vn>
2025-01-04 13:38:25 +0700

    ...

--- src/components/librarySpace/components/Playlist/Playlist.js
+++ src/components/librarySpace/components/Playlist/Playlist.js
@@ -8,7 +8,7 @@ const Playlist = ({ playlistPic, namePlaylist, onSelectedPlaylist }) => {
             <img src={playlistPic} alt='picSong'></img>
             <div className={styles.infoSong}>
                 <p className='uiSemibold'>
-                    {namePlaylist.length > 20 ? `${namePlaylist.substring(0, 20)}...` : namePlaylist}</p>
+                    </p>
                 <p className={clsx('uiRegular', 'o50')}>Playlist</p>
             </div>
         </div>
